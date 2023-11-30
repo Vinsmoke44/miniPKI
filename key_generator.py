81a85aef3c76f32d5b00b6cf8e221a8d360ea41b
bulfrek <berne.coe@gmail.com>
2023-11-30 14:13:42 +0100

    private/public

--- key_generator.py
+++ key_generator.py
@@ -1,8 +1,6 @@
 import random
 
-def est_premier(nombre, iterations=5):
-    if nombre < 2:
-        return False
+def is_prime(nombre, iterations=5):
     for _ in range(iterations):
         a = random.randint(2, nombre - 2)
         if pow(a, nombre - 1, nombre) != 1:
@@ -10,9 +8,11 @@ def est_premier(nombre, iterations=5):
     return True
 
 def generer_nombre_premier(bits):
-    nombre = random.getrandbits(bits)
-    while not est_premier(nombre):
-        nombre = random.getrandbits(bits)
+    inf = 2**bits
+    sup = int(1.2 * 2**bits)   
+    nombre = random.randint(inf, sup)   
+    while not is_prime(nombre):
+        nombre = random.randint(inf, sup)   
     return nombre
 
 def pgcd(a, b):
@@ -35,7 +35,7 @@ def generer_cles(bits):
     phi_n = (p - 1) * (q - 1)
 
     # Choix d'un exposant public e
-    e = 65537  # Généralement choisi comme 2^16 + 1 pour des raisons de performance
+    e = 65537 
 
     # Calcul de l'exposant privé d
     d = inverse_modulaire(e, phi_n)
@@ -48,8 +48,7 @@ def generer_cles(bits):
 
     return cle_publique, cle_privee
 
-# Générer une paire de clés de 1024 bits
-cle_publique, cle_privee = generer_cles(256) 
+cle_publique, cle_privee = generer_cles(512) 
 
 print("Clé publique (n, e) :", cle_publique)
 print("Clé privée (n, d) :", cle_privee)
